eb84c3fdc4dcda3bd7b665636bba229408597f42
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2022-09-05 02:02:58 +0800

    feat: updated to use the new nyan format

--- PluginList/PluginList.cs
+++ PluginList/PluginList.cs
@@ -8,20 +8,18 @@ public class PluginList : NyanPlugin
 {
     private ImmutableArray<NyanPlugin> _plugins;
 
-    public PluginList()
+    public PluginList() : base("com.Catcd.pluginList")
     {
-        ID = "com.Catcd.pluginList";
         Name = "Plugin List";
         Author = "Catcd";
         Description = "A plugin that adds a menu of all the plugins in nyan";
-        Version = new Version(0, 0, 1);
-        ReleaseVersion = ReleaseVersion.Development;
+        Version = "0.0.1 dev";
     }
 
     protected override Task OnRegister(NyanBot nyan)
     {
         _plugins = nyan.Plugins;
-        ConsoleApplication.Instance?.commands.RegisterCommand("pluginList", ShowList);
+        NyanBotInstance.Instance?.commands.RegisterCommand("pluginList", ShowList);
         nyan.Client.MessageCreated += (_, e) =>
         {
             if (e.Author.Id == nyan.Client.CurrentUser.Id || !e.Channel.IsPrivate || e.Message.Content != "Plugin List")
@@ -46,11 +44,11 @@ public class PluginList : NyanPlugin
         var sb = new StringBuilder();
         foreach (var nyanPlugin in _plugins)
         {
-            sb.AppendLine("Id: " + nyanPlugin.ID);
+            sb.AppendLine("Id: " + nyanPlugin.Id);
             sb.AppendLine("Plugin: " + nyanPlugin.Name);
             sb.AppendLine("Description: " + nyanPlugin.Description);
             sb.AppendLine("Author: " + nyanPlugin.Author);
-            sb.AppendLine("Version: " + nyanPlugin.VersionString);
+            sb.AppendLine("Version: " + nyanPlugin.Version);
             sb.AppendLine(" ");
         }
 
